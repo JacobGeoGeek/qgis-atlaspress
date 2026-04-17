38a537dc678c7375321807e8404bedad895b6cdd
Jacob C. <jacobchaar@live.ca>
2026-04-16 20:59:12 -0400

    Fix typo in error message for missing config file in load_config_file function

--- core/config/config.py
+++ core/config/config.py
@@ -6,6 +6,6 @@ def load_config_file() -> dict:
     root_dir: Path = Path(__file__).parent.parent.parent
     config_file: Path = root_dir / "config.json"
     if not config_file.exists():
-        raise FileNotFoundError(f"Cannot found the config file: {config_file}")
+        raise FileNotFoundError(f"Cannot find the config file: {config_file}")
     with config_file.open("r") as f:
         return json.load(f)
