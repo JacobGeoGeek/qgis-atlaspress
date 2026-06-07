7eac72d7ddeb93548f4b1a3f9982968423596535
Jacob C. <jacobchaar@live.ca>
2026-06-07 09:41:05 -0400

    refactor: update services to use AssetService and ShippingService, removing UploadService references

--- core/__init__.py
+++ core/__init__.py
@@ -2,15 +2,21 @@ from .product.models.product import ProductType
 from .product.product_service import ProductService
 from .product.tasks.fetch_products_by_type_task import FetchProductsByTypeTask
 from .services import CoreServices, create_core_services
-from .upload.tasks.upload_file_task import UploadFileTask
-from .upload.upload_service import UploadService
+from .assets.asset_service import AssetService
+from .assets.tasks.upload_file_task import UploadFileTask
+from .order import OrderState
+from .shipping import FetchCountriesTask, ShippingAddress, ShippingService
 
 __all__ = [
     "ProductService",
-    "UploadService",
+    "AssetService",
+    "ShippingService",
     "CoreServices",
     "create_core_services",
     "FetchProductsByTypeTask",
+    "FetchCountriesTask",
     "UploadFileTask",
     "ProductType",
+    "ShippingAddress",
+    "OrderState",
 ]
