c859d45d740dc4628a31d5fb9c205ad699cf6bb2
Jacob C. <jacobchaar@live.ca>
2026-06-14 07:58:37 -0400

    add quote service and tasks to core services initialization

--- core/services.py
+++ core/services.py
@@ -6,6 +6,8 @@ from .product.product_repository import ProductRepository
 from .product.product_service import ProductService
 from .assets.asset_repository import AssetRepository
 from .assets.asset_service import AssetService
+from .quote.quote_repository import QuoteRepository
+from .quote.quote_service import QuoteService
 from .shipping.shipping_repository import ShippingRepository
 from .shipping.shipping_service import ShippingService
 
@@ -16,6 +18,7 @@ class CoreServices:
     product_service: ProductService
     asset_service: AssetService
     shipping_service: ShippingService
+    quote_service: QuoteService
 
 
 def create_core_services() -> CoreServices:
@@ -30,4 +33,5 @@ def create_core_services() -> CoreServices:
         product_service=ProductService(ProductRepository(http_client)),
         asset_service=AssetService(AssetRepository(http_client)),
         shipping_service=ShippingService(ShippingRepository(http_client)),
+        quote_service=QuoteService(QuoteRepository(http_client)),
     )
