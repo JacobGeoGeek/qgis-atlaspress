c83bae14f950c78b32294e2d8728f970fb51f4b0
Jacob C. <jacobchaar@live.ca>
2026-06-14 08:03:53 -0400

    Add QuoteDialog to exports in __init__.py

--- ui/__init__.py
+++ ui/__init__.py
@@ -1,4 +1,5 @@
 from .product import ProductDialog
+from .quote import QuoteDialog
 from .shipping import ShippingAddressDialog
 
-__all__ = ["ProductDialog", "ShippingAddressDialog"]
+__all__ = ["ProductDialog", "QuoteDialog", "ShippingAddressDialog"]
