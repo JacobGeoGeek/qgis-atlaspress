23c3304f0e1f85dc32dcaba4617ad6350affa5e1
Jacob C. <jacobchaar@live.ca>
2026-07-14 21:41:48 -0400

    add CheckoutService and CreateCheckoutTask to core services

--- core/services.py
+++ core/services.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
 
+from .assets.asset_repository import AssetRepository
+from .assets.asset_service import AssetService
+from .checkout.checkout_repository import CheckoutRepository
+from .checkout.checkout_service import CheckoutService
 from .config.config import load_config_file
 from .config.http_client import HttpClient
 from .product.product_repository import ProductRepository
 from .product.product_service import ProductService
-from .assets.asset_repository import AssetRepository
-from .assets.asset_service import AssetService
 from .quote.quote_repository import QuoteRepository
 from .quote.quote_service import QuoteService
 from .shipping.shipping_repository import ShippingRepository
@@ -15,6 +17,7 @@ from .shipping.shipping_service import ShippingService
 @dataclass(frozen=True)
 class CoreServices:
     http_client: HttpClient
+    checkout_service: CheckoutService
     product_service: ProductService
     asset_service: AssetService
     shipping_service: ShippingService
@@ -30,6 +33,7 @@ def create_core_services() -> CoreServices:
 
     return CoreServices(
         http_client=http_client,
+        checkout_service=CheckoutService(CheckoutRepository(http_client)),
         product_service=ProductService(ProductRepository(http_client)),
         asset_service=AssetService(AssetRepository(http_client)),
         shipping_service=ShippingService(ShippingRepository(http_client)),
